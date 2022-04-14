bb7568f29793564c8531f7a8a1cbcb380ed819bd
Darci Bailey <dbailey@lcvista.com>
2022-04-13 17:45:05 -0700

    updated contact page with form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,13 +10,19 @@ const contact = () => {
     <div className="container contact__container">
       <div className="contact__options">
         <article className='contact__option'>
-          <AiOutlineMail />
+          <AiOutlineMail className='contact_option-icon' />
           <h4>Email</h4>
           <h5>darcibailey@gmail.com</h5>
           <br></br>
-          <a href='mailto:darcibailey@gmail.com'>Send a Message</a>
+          <a href='mailto:darcibailey@gmail.com'target="blank">Send an Email</a>
         </article>
       </div>
+      <form actions="">
+        <input type="text" name='name' placeholder='Full Name' required />
+        <input type="email" name='email' placeholder='Email' required />
+        <textarea name='message' placeholder='Your Message' required ></textarea>
+        <button type='submit' className='btn btn-primary'>Send Message</button>
+      </form>
     </div>
     </section>
   )
